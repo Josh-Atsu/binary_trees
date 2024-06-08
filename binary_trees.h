2ae27d6a200e59b2fe8b8dc1ff4ead7d6c77b1b1
Joshua <atsujoshua67@gmail.com>
2024-06-08 13:27:35 -0700

    correction

--- binary_trees.h
+++ binary_trees.h
@@ -24,24 +24,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
-
-/**
- * struct queue_list_s - linked list structure
- *
- * @value: integer stored in the list
- * @next: pointer to the next node index
- */
-struct queue_list_s
-{
-	int value;
-	struct queue_list_s next;
-};
-
-typedef struct queue_list_s queue_list_t;
 
 /* Prototypes */
 void binary_tree_print(const binary_tree_t *);
@@ -70,27 +52,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int calculate_depth(const binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-int binary_tree_is_complete(const binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-int binary_tree_is_bst(const binary_tree_t *tree);
-bst_t *bst_insert(bst_t **tree, int value);
-bst_t *array_to_bst(int *array, size_t size);
-bst_t *bst_search(const bst_t *tree, int value);
-bst_t *bst_remove(bst_t *root, int value);
-int binary_tree_is_avl(const binary_tree_t *tree);
-avl_t *avl_insert(avl_t **tree, int value);
-avl_t *array_to_avl(int *array, size_t size);
-avl_t *avl_remove(avl_t *root, int value);
-avl_t *sorted_array_to_avl(int *array, size_t size);
-int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value);
-heap_t *array_to_heap(int *array, size_t size);
-int heap_extract(heap_t **root);
-int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
-/* prototype for Queues */
-queue_list_t *enqueue(queue_list_t *queue, int value);
-
 
 #endif /* _BINARY_TREES_H_ */
